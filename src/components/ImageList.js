abe74ab3ccccce5713b9c17d6a7ba4faa318f40e
stephensmakilan <stephensmakilan@gmail.com>
2021-03-07 22:04:58 +0800

    adding Grid CSS

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Imagelist.css";
 
 const ImageList = props => {
     const images = props.images.map(({description, id, urls}) => {
-            return <img alt={description} key={id} src={urls.regular}/>
+            return <img alt={description} key={id} src={urls.full}/>
         });
 
     return (
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     )
 }
  
